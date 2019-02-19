38152d5700d905bd7e82a78d2238c10d6d7de72f
Song Young Jin <subutuo@naver.com>
2019-02-19 21:18:18 +0900

    TO-DO 기록앱

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,67 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <task-header></task-header>
+    <task-input v-on:addTask="addTask"></task-input>
+    <task-list v-bind:propsdata="items" v-on:removeTask="removeTask"></task-list>
+    <task-footer v-on:clearTask="clearTask"></task-footer>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  import TaskHeader from "./components/taskHeader";
+  import TaskInput from "./components/taskInput";
+  import TaskList from "./components/taskList";
+  import TaskFooter from "./components/taskFooter";
+
+  export default {
+    data() {
+      return {
+        items: []
+      }
+    },
+    methods: {
+      addTask(item) {
+        localStorage.setItem(item, item);
+        this.items.push(item);
+      },
+      removeTask(item, index) {
+        localStorage.removeItem(item);
+        this.items.splice(index, 1);
+      },
+      clearTask() {
+        localStorage.clear();
+        this.items=[];
+      }
+    },
+    created() {
+      if(localStorage.length>0) {
+        for(let i=0; i<localStorage.length; i++) {
+          this.items.push(localStorage.key(i));
+        }
+      }
+    },
+    components: {
+      'task-header': TaskHeader,
+      'task-input': TaskInput,
+      'task-list': TaskList,
+      'task-footer': TaskFooter
     }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+  body {
+    text-align: center;
+    background-color: #F6F6F8;
+  }
+  input {
+    border-style: groove;
+    width: 200px;
+  }
+  button {
+    border-style: groove;
+  }
+  .shadow {
+    box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03)
+  }
 </style>
